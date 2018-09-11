b121f009992de5960160a6864017bc81b4b7e3d8
Prashant Chaudhari <prashantchaudhari881@gmail.com>
2018-09-11 23:58:42 +0530

    Added animation & Fixed some fonts
    Added Animation to Hamburger menu & and fixed spacing for multiple devices.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const toggle = document.querySelector('.nav-toggle');
 
 function toggleClass() {
   const navigation = document.querySelector('.main-nav');
+  const hamburger = document.querySelector('.hamburger');
 
   navigation.classList.toggle('is-open');
+  hamburger.classList.toggle('is-open');
 }
 
 toggle.addEventListener('click', toggleClass);
